94a32d5d9bbc36478b8968e80b1d5c139f3945a2
phyo110856 <phyominkhant106@gmail.com>
2023-11-06 00:06:21 +0630

    updated

---
+++ js/nt.js
@@ -0,0 +1,13 @@
+const myName = "phyo min khant";
+console.log(myName);
+
+let x = 10;
+let y = 20;
+console.log(x > y);
+
+let arr = ["a","b","c"];
+console.log(arr);
+
+for (x of arr){
+    console.log(x);
+}
